bd09760762812c45e0d4b08d7d05cde538ba08d0
amauryhochart <amaury.hochart25@gmail.com>
2024-03-14 18:42:37 +0100

    feat(MEEDZPROJECTID-XXXX) : export pdf works in docker and use of ENV variables SPRING_PROFILE

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM openjdk:21
 VOLUME /tmp
 COPY target/*.jar app.jar
+COPY src/main/resources /resources
+#SPRING_PROFILE=local for dev use
+ENV SPRING_PROFILE=prod
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/org/meedz/cvblockformapi/service/CvBlockFormService.java
+++ src/main/java/org/meedz/cvblockformapi/service/CvBlockFormService.java
@@ -2,25 +2,43 @@ package org.meedz.cvblockformapi.service;
 
 import com.lowagie.text.DocumentException;
 import org.meedz.cvblockformapi.model.SkillFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Service
 public class CvBlockFormService {
+
+    Logger logger = LoggerFactory.getLogger(Logger.class);
+
+    @Value("${spring.profile}")
+    String activeSpringProfile;
 
     public File generatePdfFromHtml(SkillFolder skillFolder) {
         String outputFolder = "exports_pdf" + File.separator + "thymeleaf.pdf";
         OutputStream outputStream = null;
         try {
-            outputStream = new FileOutputStream(outputFolder);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("FILE NOT FOUND");
+            if ("local".equalsIgnoreCase(activeSpringProfile)) {
+                outputStream = new FileOutputStream(outputFolder);
+            } else {
+                File file = ResourceUtils.getFile("/resources/" + outputFolder);
+                outputStream = new FileOutputStream(file);
+            }
+        } catch (IOException e) {
+            logger.error("FILE NOT FOUND " + e);
+            throw new RuntimeException("FILE NOT FOUND " + e);
         }
 
         ITextRenderer renderer = new ITextRenderer();
@@ -52,9 +70,14 @@ public class CvBlockFormService {
 
         try {
             outputStream.close();
-            return new File(outputFolder);
+            if ("local".equalsIgnoreCase(activeSpringProfile)) {
+                return new File(outputFolder);
+            } else {
+                return ResourceUtils.getFile("/resources/" + outputFolder);
+            }
         } catch (IOException e) {
-            throw new RuntimeException("IO Error to close");
+            logger.error("IO Error to close " + e);
+            throw new RuntimeException("IO Error to close " + e);
         }
     }
 
